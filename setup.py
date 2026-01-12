90304afcf209ae70bb1cecbf6ee9a2be7738430d
rahul-exo <ma-66@MA-66s-MacBook-Air.local>
2026-01-12 13:11:00 +0530

    Initial project structure using setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="house-price-prediction",
+    version="0.0.1",
+    author="Ramakrishna Thakur",
+    author_email="your_email@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
